f97bfb05dd2f852c75f1c4390821e9f90d115415
Aleksandr <froteru955@gmail.com>
2024-11-23 13:10:00 +0300

    DZ13 dop primer

--- DZ/src/DZ13/Main1.java
+++ DZ/src/DZ13/Main1.java
@@ -1,4 +1,14 @@
 package DZ13;
 
 public class Main1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, -3, 6, -7, -11};
+        System.out.println("n = " + calcNegativeNumbers(arr, 0));
+    }
+
+    static int calcNegativeNumbers(int[] numbers, int index) {
+        if (index == numbers.length)
+            return 0;
+        return (numbers[index] < 0 ? 1 : 0) + calcNegativeNumbers(numbers, index + 1);
+    }
 }
