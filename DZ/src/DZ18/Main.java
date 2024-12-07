e036d6908ef1bbb762dcf34e72b2146dfa05fcf6
Aleksandr <froteru955@gmail.com>
2024-12-07 17:03:15 +0300

    DZ 18

--- DZ/src/DZ18/Main.java
+++ DZ/src/DZ18/Main.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        String[] numberIphoneArray = {"+7 499 555-75-33", "+74995557533", "7(499)555 75 33", "7 (499) 555-75 33", "7r499 555 75-33", "7 499 55-75-33", "+7 r99 555-75-33", "+7 499 555-75-3354"};
+        String[] numberIphoneArray = {"+7 499 555-75-33", "+74995557533", "7(499) 555 75 33", "7 (499) 555-75 33","7r499 555 75-33", "7 499 55-75-33", "+7 r99 555-75-33", "+7 499 555-75-3354"};
         String pattern = "^\\+?7\\s*\\(?\\d{3}\\)?\\s*\\d{3}(\\s*-?\\d{2}){2}$";
         Pattern regex = Pattern.compile(pattern);
         Matcher matcher;
