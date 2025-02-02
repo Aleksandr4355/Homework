5fb1cf418272a84958cbdb0dac9ba5bb9c966020
Aleksandr <froteru955@gmail.com>
2025-02-02 11:55:14 +0300

    DZ32

---
+++ DZ/src/DZ32/Main.java
@@ -0,0 +1,16 @@
+package DZ32;
+
+import java.util.*;
+
+public class Main extends TreeSet {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.print("-> ");
+            treeSet.add(scan.nextInt());
+        }
+        System.out.println("Наименьший элемент в TreeSet: " + treeSet.first());
+        System.out.println(treeSet);
+    }
+}
